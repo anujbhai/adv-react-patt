7be060e76b446f87bda3c98df55ef73ec12acf76
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-02-02 23:32:05 +0530

    clean-slate - styling components.

--- showcase/webpack.config.base.js
+++ showcase/webpack.config.base.js
@@ -11,7 +11,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         options: {
